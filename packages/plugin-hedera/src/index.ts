8b13695419c35782357948ce94778f71d50a2939
mateuszm-arianelabs <mateusz.marcinkowski@arianelabs.com>
2025-02-05 10:01:56 +0100

    feat: create action for fetching topics info (CCS-93) (#12)
    * feat: create action for fetching topics info (CCS-93)
    
    Signed-off-by: Mateusz Marcinkowski <mateusz.marcinkowski@arianelabs.com>
    
    * feat: add docs and minor improvements (CCS-105)
    
    Signed-off-by: Mateusz Marcinkowski <mateusz.marcinkowski@arianelabs.com>
    
    ---------
    
    Signed-off-by: Mateusz Marcinkowski <mateusz.marcinkowski@arianelabs.com>

--- packages/plugin-hedera/src/index.ts
+++ packages/plugin-hedera/src/index.ts
@@ -14,6 +14,7 @@ import { claimAirdropAction } from "./actions/claim-airdrop/claim-airdrop.ts";
 import { transferTokenAction } from "./actions/transfer-token/transfer-token.ts";
 import { createTopicAction } from "./actions/create-topic/create-topic.ts";
 import { deleteTopicAction } from "./actions/delete-topic/delete-topic.ts";
+import { topicInfoAction } from "./actions/topic-info/topic-info.ts";
 
 export const hederaPlugin: Plugin = {
     name: "Hedera",
@@ -36,6 +37,7 @@ export const hederaPlugin: Plugin = {
         transferTokenAction,
         createTopicAction,
         deleteTopicAction,
+        topicInfoAction,
     ],
 };
 
