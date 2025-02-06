74bdc449c5280edffa25b5a667b2813a211fad2b
Mateusz Marcinkowski <mateusz.marcinkowski@arianelabs.com>
2025-02-06 12:41:34 +0100

    feat: implement option to create fungible token with supply key set (CCS-106)
    Signed-off-by: Mateusz Marcinkowski <mateusz.marcinkowski@arianelabs.com>

--- packages/plugin-hedera/src/actions/create-token/schema.ts
+++ packages/plugin-hedera/src/actions/create-token/schema.ts
@@ -5,4 +5,5 @@ export const createTokenParamsSchema = z.object({
     symbol: z.string(),
     decimals: z.coerce.number(),
     initialSupply: z.coerce.number(),
-})
+    isSupplyKey: z.boolean(),
+});
